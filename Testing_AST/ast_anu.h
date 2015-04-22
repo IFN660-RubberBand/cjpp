3c39df880be7d914bf76954dd6b34fcc63f6e0de
Anu <anu_28oct92@yahoo.co.in>
2015-04-22 15:05:48 +1000

    Create ast_anu.h

---
+++ Testing_AST/ast_anu.h
@@ -0,0 +1,90 @@
+class Expression
+{
+ virtual ~Expression() {}
+virtual Expression *clone () = 0;
+virtual int value () = 0;
+};
+
+
+class Plus :public Expression{
+
+Expression *m_left, *m_right;
+
+public: Plus(Expression *left, Expression *right): m_left (left), m_right (right) {}
+
+Plus(const Plus &other){
+	m_left = other.n_left->clone();
+	m_right = other.m_right->clone();
+	}
+	
+	virtual ~Plus()
+	{
+		delete m_left;
+		delete m_right;
+	}
+	
+	Plus &operator = (const Plus &other) {
+		 if (&other != this) {
+      delete m_left;
+      delete m_right;
+      
+      m_left = other.m_left->clone ();
+      m_right = other.m_right->clone ();
+    }
+	}
+	
+	
+  virtual Expression *clone () { return new Plus (*this); }
+  
+  virtual int value () { return m_left->value () * m_right->value (); }
+	
+};
+
+class Number :public Expression {
+	int m_val;
+
+public:
+   
+  Number (int val): m_val (val) {}
+
+  // Copy constructor
+  Number (const Number &other) { m_val = other.m_val; }
+
+  Number &operator = (const Number &other) {
+    if (&other != this)
+      m_val = other.m_val;
+  }
+
+  virtual Expression *clone () { return new Number (*this); }
+
+  virtual int value () { return m_val; }
+
+
+};
+
+
+class Ident : public Expression {
+
+ int *m_val;
+
+public:
+   
+  Ident (int *val): m_val (val) {}
+
+  // Copy constructor
+  Ident (const Ident &other) { m_val = other.m_val; }
+
+  Ident &operator = (const Ident &other) {
+    if (&other != this)
+      m_val = other.m_val;
+  }
+
+  virtual Expression *clone () { return new Ident (*this); }
+  
+
+  virtual int value () { return *m_val; }
+
+
+
+
+};
