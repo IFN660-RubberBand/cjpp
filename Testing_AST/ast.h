aaaa15f6b8d5516c56778dd1c8e372b9b9bae73d
linton <lintonpinto@gmail.com>
2015-04-22 13:58:24 +1000

    AST logic for multiplication of two numbers

--- Testing_AST/ast.h
+++ Testing_AST/ast.h
@@ -9,29 +9,43 @@ class Expression {
 }; 
 
 
-// code for Plus class goes here
-
-
-
-
-//code for Subt class goes here
-
-
-
-
-
-
-
-
 
 class Times : public Expression {
 
 
+Expression *m_left, *m_right;
+
+public:
+   
+  Times (Expression *left, Expression *right): m_left (left), m_right (right) {}
+
+  // Copy constructor
+  Times (const Times &other) {
+    m_left = other.m_left->clone ();
+    m_right = other.m_right->clone ();
+  }
+
+  virtual ~Times () 
+  {
+    delete m_left;
+    delete m_right;
+  }
 
 
+ Times &operator = (const Times &other) {
+    if (&other != this) {
+      delete m_left;
+      delete m_right;
+      
+      m_left = other.m_left->clone ();
+      m_right = other.m_right->clone ();
+    }
+  }
 
 
-
+  virtual Expression *clone () { return new Times (*this); }
+  
+  virtual int value () { return m_left->value () * m_right->value (); }
 
 
 
