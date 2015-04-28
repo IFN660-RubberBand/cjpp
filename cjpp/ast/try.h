d4138921d38419bfd259cffa3f2dda527d2e5898
Anu <anu_28oct92@yahoo.co.in>
2015-04-28 15:35:52 +1000

    Updated Try Catch Finally

--- cjpp/ast/try.h
+++ cjpp/ast/try.h
@@ -1,6 +1,26 @@
-#include "ast.h"
+#include "Node.h"
 
 class TryStatement_node : public Statement_node{
 public:
+  NExpression* expr;
+	NTryStatement(const NExpression* e,const NStatement* try, const NStatement* catch, const NStatement* finally, const NStatement* block)
+    : NStatement (try), NStatement(block), NStatement (catch), NStatemnt (finally)
+	{}
+};
 
+class NCatchStatement : public Statement_node{
+public:
+	NIdentifier* id;
+	NCatchStatement( const NIdentifier* i, const NStatement* catch)
+	:NStatement (catch), id(i), NStatement(Block)
+	{}
+	
+};
+
+class NFinallyStatement : public Statement_node{
+public:
+	NFinallyStatement(const NStatement* finally, const Nstatemnt* block)
+	:NStatement (finally), NStatement(block)
+	{}
+	
 };
