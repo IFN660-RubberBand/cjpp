8aa2084c7fe32d535c71f850d8db0fc8843c7528
Jeevan <jeevan.basavanna@gmail.com>
2015-04-28 15:33:50 +1000

    Updated Return Statement

--- cjpp/ast/return.h
+++ cjpp/ast/return.h
@@ -1,6 +1,10 @@
-#include "ast.h"
+#include "Node.h"
 
 class ReturnStatement_node : public Statement_node{
 public:
+NExpression* expr;
+	NReturnStatement(const NExpression* e, const NStatement* return)
+	: NStatement(return), expr(e)
+	{}
 
 };
