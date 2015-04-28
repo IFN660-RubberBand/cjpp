01b58c83c0a43e86b0183ae0b82d7630f2212a49
Anu <anu_28oct92@yahoo.co.in>
2015-04-28 15:37:36 +1000

    Updated Throw

--- cjpp/ast/throw.h
+++ cjpp/ast/throw.h
@@ -2,5 +2,9 @@
 
 class ThrowStatement_node : public Statement_node{
 public:
+NExpression* expr;
+	NThrowStatement(const NExpression* e, const NStatement* throw)
+	: NStatement(throw), expr(e)
+	{}
 
 };
