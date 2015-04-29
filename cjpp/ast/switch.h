f4fea18e3d45a243a0e6e867426d799bbc7a2e76
Jeevan <jeevan.basavanna@gmail.com>
2015-04-29 10:04:02 +1000

    Updated Switch Statement

--- cjpp/ast/switch.h
+++ cjpp/ast/switch.h
@@ -1,42 +1,13 @@
 #include "Node.h"
+#include "Caseblock.h"
 
-class NSwitchStatement : public NStatement {
+class SwitchStatement : public Statement {
 public:
-	NExpression* expr;
-	NSwitchStatement(const NExpression* e, const NStatement* switch, const NStatement* caseblock)
-	: NStatement(switch), nStatement(caseblock), expr(e)
+	const Expression* expr;
+	Caseblock* cb;
+	SwitchStatement(const Expression* e, const Statement* switch, Caseblock* c)
+	: expr(e),cb(c);
 	{}
 };
 
-class NCaseblockStatement : public NStatement {
-public:
-	NCaseblock(const NStatement* defaultclause, const NStatement* caseclause)
-	: NStatement(caseclause), NStatement(defaultclause)
-	{}
-	
-};
-
-class NCaseclausesStatement : public NStatement {
-public:
-	NCaseclauses(const NStatement* caseclauses, const NStatement* caseclause)
-	: NStatement(caseclauses), NStatement(caseclause)
-	{}
-	
-};
-
-class NCaseclauseStatement : public NStatement {
-public:
-	NCaseclause(const NExpression* e, const NStatement* caseclause, const NStatement* stmtlist)
-	: NStatement(caseclause), NStatement(stmtlist), expr(e)
-	{}
-	
-};
-
-class NDefaultclauseStatement : public NStatement {
-public:
-	NDefaultclause(const NStatement* default, const NStatement* stmtlist)
-	: NStatement(default), NStatement(stmtlist),
-	{}
-	
-};
 
