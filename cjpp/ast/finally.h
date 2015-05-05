cb9d624d391c67772b1331869a1bd043a186534e
Anu <anu_28oct92@yahoo.co.in>
2015-05-05 11:19:32 +1000

    Update and rename finally.h to Finally.h

--- cjpp/ast/finally.h
+++
@@ -1,9 +0,0 @@
-#include "Node.h"
-
-class NFinallyStatement : public Statement_node{
-public:
-	NFinallyStatement(const NStatement* finally, const Nstatemnt* block)
-	:NStatement (finally), NStatement(block)
-	{}
-	
-};
