8c924948c5e35b27558abaaa5d7017c9d61cc30a
Sebastian <sebastianherber@web.de>
2015-05-06 17:08:01 +1000

    began implementing dynamic name resolution but FAILS

--- cjpp/helpclasses/test.c
+++ cjpp/helpclasses/test.c
@@ -1,9 +1,23 @@
 #include "value.h"
-
+#include <stdio.h>
+#include <string>
+#include <iostream>
 
 int main(int argc, char* argv[]) {
 	
-	Value* i = new IntegerValue(42);
+	char* var = "i";
+	Value* cont = new IntegerValue(42);
+	
+	cout << "point 1" << endl;
+	ObjectValue obj;
+	cout << "point 2" << endl;
+	
+	
+	obj.set(var, cont);
+	Value* tmp = obj.resolve(var);
+	
+	cout << &(*cont) << endl;
+	cout << &(*tmp);
 	
 	
 	return 0;
--- cjpp/helpclasses/value.h
+++ cjpp/helpclasses/value.h
@@ -1,14 +1,65 @@
 #include <stdint.h>
+#include <map>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+
+#ifndef VALUE
+#define VALUE
+
+using namespace std;
+
 
 
 class Value {
+	virtual int getType() = 0;
 };
 
+class UndefinedValue : public Value {
+	int getType() { return 0; } 
+};
 
 class IntegerValue : public Value {
 public:
 	int32_t v;
 	IntegerValue(int32_t v) : v(v)
 	{}
+	int getType() { return 1; } 
+};
+
+class ObjectValue : public Value {
+public:
+	map<char*, Value*>* m;
+	ObjectValue() {
+		cout << "constructor";
+		m = new map<char*, Value*>();
+		(*m)["i"] = 42;
+		auto search = m->find("i");
+    	if(search != m->end()) {
+        	cout << "found";
+    	} else {
+        	cout << "not found";
+    	}
+	}
+	int getType() { return 2;
+	}
+	Value* resolve(char* ident) {
+		auto search = m->find(ident);
+    	if(search != m->end()) {
+        	return search->second;
+    	} else {
+        	return new UndefinedValue();
+    	}
+	}
+	void set(char* ident, Value* value) {
+		//TODO delete old value of map[ident]
+		(*m)[ident] = value;
+	}	
+
 	
 };
+
+
+
+
+#endif
