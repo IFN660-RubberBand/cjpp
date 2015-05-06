2030f08fb63e80a751c1c75520494b388873cb9e
Zipps <zipps.dev@gmail.com>
2015-05-06 16:23:35 +1000

    Generated code outputs to file

--- cjpp/ast/program.h
+++ cjpp/ast/program.h
@@ -17,10 +17,20 @@ public:
 		cout << endl << endl;
 	}
 	void generateCode() const {
-		cout << "Generated code:" << endl;
+		cout << "Generated code in file 'output.c'" << endl;
+		std::streambuf *psbuf, *backup;
+		std::ofstream myfile;
+		myfile.open ("output.c");
+		// redirect cout to file
+		backup = std::cout.rdbuf();
+		psbuf = myfile.rdbuf();
+		std::cout.rdbuf(psbuf);
+		//
 		cout << "int main(int argc, char* argv[]) {" << endl;
 		l->generateCode();
 		cout << "return 0;" << endl << "}" << endl; 
+		std::cout.rdbuf(backup);
+		myfile.close();
 	}
 	
 };
