f40df3a7b1e271f4100719456841e2786a656157
Sebastian <sebastianherber@web.de>
2015-05-06 18:37:12 +1000

    fixed dynamic resolving for objects

--- cjpp/helpclasses/test.c
+++ cjpp/helpclasses/test.c
@@ -5,20 +5,34 @@
 
 int main(int argc, char* argv[]) {
 	
-	char* var = "i";
-	Value* cont = new IntegerValue(42);
+	char* var1 = "foo";
+	char* var2 = "foo";
+	char* var3 = "bar";
+	Value* content = new IntegerValue(42);
 	
-	cout << "point 1" << endl;
-	ObjectValue obj;
-	cout << "point 2" << endl;
+	ObjectValue* obj = new ObjectValue();
+	obj->set(var1, content);
 	
 	
-	obj.set(var, cont);
-	Value* tmp = obj.resolve(var);
+	cout << "is it working?" << endl;
+	cout << &(*content) << endl; //the address of the original object
+		
+	Value* tmp1 = obj->resolve(var2); //resolves to the same object
+	cout << &(*tmp1) << endl;		  //notice that set is called with var1 but resolve with var2 !!!
 	
-	cout << &(*cont) << endl;
-	cout << &(*tmp);
+	Value* tmp2 = obj->resolve(var3);	//doesnt resolve, returns pointer to UndefinedObject
+	cout << &(*tmp2) << endl;	 
 	
+	obj->set(var2, tmp2);			//overwrite the value of foo 
+	tmp2 = obj->resolve(var1);		//deletes the old value
+	cout << &(*tmp2) << endl;		//now also points to the undefined object
+	
+	obj->set(var3, new IntegerValue(11));
+	tmp2 = obj->resolve(var3);			//now it does resolve
+	cout << &(*tmp2) << endl;			//new pointer value
+	
+	tmp2 = obj->resolve(var2);		//foo still points to the undefined object
+	cout << &(*tmp2) << endl;		
 	
 	return 0;
 } 
--- cjpp/helpclasses/value.h
+++ cjpp/helpclasses/value.h
@@ -12,11 +12,10 @@ using namespace std;
 
 
 class Value {
-	virtual int getType() = 0;
+
 };
 
 class UndefinedValue : public Value {
-	int getType() { return 0; } 
 };
 
 class IntegerValue : public Value {
@@ -24,25 +23,15 @@ public:
 	int32_t v;
 	IntegerValue(int32_t v) : v(v)
 	{}
-	int getType() { return 1; } 
 };
 
 class ObjectValue : public Value {
 public:
 	map<char*, Value*>* m;
 	ObjectValue() {
-		cout << "constructor";
 		m = new map<char*, Value*>();
-		(*m)["i"] = 42;
-		auto search = m->find("i");
-    	if(search != m->end()) {
-        	cout << "found";
-    	} else {
-        	cout << "not found";
-    	}
 	}
-	int getType() { return 2;
-	}
+	
 	Value* resolve(char* ident) {
 		auto search = m->find(ident);
     	if(search != m->end()) {
@@ -51,11 +40,15 @@ public:
         	return new UndefinedValue();
     	}
 	}
-	void set(char* ident, Value* value) {
-		//TODO delete old value of map[ident]
-		(*m)[ident] = value;
-	}	
 
+	void set(char* ident, Value* value) {
+		auto search = m->find(ident);
+    	if(search != m->end()) {
+			cout << "delete old object" << endl;	
+			//delete search->second;
+		}
+        (*m)[ident] = value;
+	}
 	
 };
 
