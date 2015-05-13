4a37db7ad6bde8c4eed3e5137afc8739102cd526
Adrian Zborowski <adrian.zborowski@hva.nl>
2015-05-13 14:23:23 +1000

    Removed the deprecated functions. Added JavaScript style Subtraction of values. Removed the not needed endl's. + Added subtraction + Cleaned the code - Removed the not needed endl's

--- cjpp/helpclasses/MathFuncsLib.h
+++ cjpp/helpclasses/MathFuncsLib.h
@@ -5,42 +5,6 @@ using namespace std;
 
 class MathFuncs{
 public:
-	
-	/*
-	 * Deprecated
-	 */
-	static double Add(double lval, double rval){
-		return lval + rval;
-	};
-
-	/*
-	 * Deprecated
-	 */
-	static string AddString(string lval, string rval){
-		return lval + rval;
-	};
-
-	/*
-	 * Deprecated
-	 */
-	template<typename L, typename R>
-	static auto AddObject(L lval, R rval) -> decltype(lval + rval){
-		cout << lval << ":" << rval << endl;
-		return lval + rval;
-	};
-
-	template<typename L, typename R>
-	static auto do_add(L lval, R rval, false_type){
-		return lval + rval;
-	}
-
-	template<typename L, typename R>
-	static string do_add(L lval, R rval, true_type){
-		ostringstream oss;
-		oss << lval << rval;
-		return oss.str();
-	}
-
 	/*
 	 * Addition function.
 	 */
@@ -52,7 +16,41 @@ public:
 			|| is_convertible<R, string>{} >;
 		return do_add(lval, rval, dispatch_type());
 	}
-
+    
+    template<typename L, typename R>
+    static auto do_add(L lval, R rval, false_type){
+        return lval + rval;
+    }
+    
+    template<typename L, typename R>
+    static string do_add(L lval, R rval, true_type){
+        ostringstream oss;
+        oss << lval << rval;
+        return oss.str();
+    }
+        
+    /*
+     * Subtraction function.
+     */
+    template<typename L, typename R>
+    static auto sub(L lval, R rval){
+        using dispatch_type = integral_constant<
+            bool,
+            is_convertible<L, string>{}
+            || is_convertible<R, string>{} >;
+        return do_sub(lval, rval, dispatch_type());
+    }
+    
+    template<typename L, typename R>
+    static auto do_sub(L lval, R rval, false_type){
+        return lval - rval;
+    }
+    
+    template<typename L, typename R>
+    static string do_sub(L lval, R rval, true_type){
+        return "NaN";
+    }
+    
 	/*
 	* Comparators
 	*/
--- cjpp/helpclasses/mainCalcTest.cpp
+++ cjpp/helpclasses/mainCalcTest.cpp
@@ -12,29 +12,31 @@ int main(){
 
 	int num1 = 4;
 	int num2 = 2;
-
-	// cout << "(Old) Only numbers: " << MathFuncs::Add(1, 2) << endl << endl;
-	// cout << "(Old) Only strings: " << MathFuncs::AddString("Left Val", "Right Val") << endl << endl;
-
-	// cout << MathFuncs::MathFuncs::AddObject("Left Val", "Right Val") << endl;
-	// cout << MathFuncs::AddObject("Test", num2) << endl;
 	// cout << "Is of type: " << typeid((MathFuncs::AddObject("Test", num2))).name() << endl;
 
-	cout << "Additions" << endl << endl;
-	cout << "string + integer: " << MathFuncs::add("Hai", 4) << endl << endl;
-	cout << "integer + string: " << MathFuncs::add(4, "Hai") << endl << endl;
-	cout << "string + double: " << MathFuncs::add("Hai", 4.2) << endl << endl;
-	cout << "double + string: " << MathFuncs::add(4, "Hai") << endl << endl;
-	cout << "string + string: " << MathFuncs::add("Hello", "World!") << endl << endl;
-	cout << "integer + integer: " <<MathFuncs::add(2, 4) << endl << endl;
+	cout << endl << "> Additions" << endl;
+	cout << "string + integer: " << MathFuncs::add("Hai", 4) << endl;
+	cout << "integer + string: " << MathFuncs::add(4, "Hai") << endl;
+	cout << "string + double: " << MathFuncs::add("Hai", 4.2) << endl;
+	cout << "double + string: " << MathFuncs::add(4, "Hai") << endl;
+	cout << "string + string: " << MathFuncs::add("Hello", "World!") << endl;
+	cout << "integer + integer: " <<MathFuncs::add(2, 4) << endl;
+    
+    cout << endl << "> Subtraction" << endl;
+    cout << "string - integer: " << MathFuncs::sub("Hai", 4) << endl;
+    cout << "integer - string: " << MathFuncs::sub(4, "Hai") << endl;
+    cout << "string - double: " << MathFuncs::sub("Hai", 4.2) << endl;
+    cout << "double - string: " << MathFuncs::sub(4, "Hai") << endl;
+    cout << "string - string: " << MathFuncs::sub("Hello", "World!") << endl;
+    cout << "integer - integer: " <<MathFuncs::sub(2, 4) << endl;
 
-	cout << "Comparators" << endl << endl;
-	cout << "2 < 4: " << MathFuncs::lssthan(2, 4) << endl << endl;
-	cout << "2.4 < 2.5: " << MathFuncs::lssthan(2.4, 2.5) << endl << endl;
-	// cout << "2 > 4: " << MathFuncs::gtrthan(2, 4) << endl << endl;
-	// cout << "2 == 2: " << MathFuncs::equalto(2, 2) << endl << endl;
-	// cout << "2 <= 2: " << MathFuncs::leqthan(2, 2) << endl << endl;
-	// cout << "integer === string: " << MathFuncs::cmp(2, "2", IDENT) << endl << endl;
+	cout << endl << "Comparators" << endl;
+	cout << "2 < 4: " << MathFuncs::lssthan(2, 4) << endl;
+	cout << "2.4 < 2.5: " << MathFuncs::lssthan(2.4, 2.5) << endl;
+	// cout << "2 > 4: " << MathFuncs::gtrthan(2, 4) << endl;
+	// cout << "2 == 2: " << MathFuncs::equalto(2, 2) << endl;
+	// cout << "2 <= 2: " << MathFuncs::leqthan(2, 2) << endl;
+	// cout << "integer === string: " << MathFuncs::cmp(2, "2", IDENT) << endl;
 
 	printf("\nEND\n");
 
