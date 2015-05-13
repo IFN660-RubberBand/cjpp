6054f82537b255478409fa6382623c52ad680795
Adrian Zborowski <adrian.zborowski@hva.nl>
2015-05-13 14:48:47 +1000

    Removed the commented typeid usage and replaced it with an comment description. + Added typeid description - Removed the typeid usage function

--- cjpp/helpclasses/mainCalcTest.cpp
+++ cjpp/helpclasses/mainCalcTest.cpp
@@ -7,12 +7,15 @@ using namespace std;
 
 /*
  * Compile: g++ -x c++ -std=c++14 mainCalcTest.cpp -o mainCalcTest && ./mainCalcTest
+ * Type checking is possible with "typeid(...).name()" (replace the "..." with your input). All the used 
+ * values need to be allocated so you can't use an integer as a value but you can use a predefined 
+ * variable with a number as a value. The given output is not human readable so you need to convert the
+ * compiled program using c++filt -t. Example: "./mainCalcTest | c++filt -t".
  */
 int main(){
 
 	int num1 = 4;
 	int num2 = 2;
-	// cout << "Is of type: " << typeid((MathFuncs::AddObject("Test", num2))).name() << endl;
 
 	cout << endl << "> Additions" << endl;
 	cout << "string + integer: " << MathFuncs::add("Hai", 4) << endl;
@@ -21,6 +24,8 @@ int main(){
 	cout << "double + string: " << MathFuncs::add(4, "Hai") << endl;
 	cout << "string + string: " << MathFuncs::add("Hello", "World!") << endl;
 	cout << "integer + integer: " <<MathFuncs::add(2, 4) << endl;
+    cout << "double + integer: " <<MathFuncs::add(2.0, 4) << endl;
+    cout << "integer + double: " <<MathFuncs::add(2, 4.0) << endl;
     
     cout << endl << "> Subtraction" << endl;
     cout << "string - integer: " << MathFuncs::sub("Hai", 4) << endl;
