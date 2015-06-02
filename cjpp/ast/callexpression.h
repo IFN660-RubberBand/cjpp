3740821185867963fb14ce0b96e6c9967e038296
Zipps <zipps.dev@gmail.com>
2015-06-02 16:31:35 +1000

    Console.log & invoking functions
    console.log() now generates code. Also added code generation for
    invoking functions

--- cjpp/ast/callexpression.h
+++ cjpp/ast/callexpression.h
@@ -5,20 +5,38 @@
 
 class CallExpression : public Expression {
 public:
-	const Expression* mexpr;
+	const Identifier* ident;
 	const Expression* args;
-	CallExpression(const Expression* mexpr, const Expression* args)
-	: mexpr(mexpr), args(args)
+	CallExpression(const Identifier* ident, const Expression* args)
+	: ident(ident), args(args)
 	{}
 	~CallExpression() {
-		delete mexpr;
+		delete ident;
 		delete args;
 	}
 	void print(unsigned int tabs) const {
 		Expression::print(tabs);
+		cout << "function call ";
+		ident->print(tabs);
+		cout << "(";
+		args->print(tabs);
+		cout << ")";
 	}
-	bool generateCode() const {
 
+	bool generateCode(TempVariable* result) const {
+		TempVariable* t = TempVariableFactory::getTemp();
+		bool del0 = args->generateCode(t);
+
+		string logfunc = "console.log";
+		string name = ident->toString();
+
+		if (logfunc == name) {
+			cout << "cout << " << t->toString() << " << endl;" << endl;
+		}
+		else {
+			cout << "currentscope->resolve(\"" << ident->toString() << "\", " << t->toString() << ")" << endl; 
+		}
+		
 	}
 };
 
